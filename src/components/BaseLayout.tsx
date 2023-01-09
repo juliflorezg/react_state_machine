67053e54da0f58c4d39cdd3b1d60a5a47ce195ff
juliflorezg <forezjuli08@hotmail.com>
2023-01-09 09:44:07 -0500

    log state

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -7,5 +7,9 @@ export const BaseLayout = () => {
 
   console.log('Our machine:', state)
 
+  console.log('this matches to true', state.matches('initial'))
+  console.log('this matches to false', state.matches('tickets'))
+  console.log('can execute FINISH', state.can('FINISH'))
+
   return <div>Base Layout</div>
 }
