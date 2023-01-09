0cd48273f0ac7a56b83dbf44e3387fb061f88757
juliflorezg <forezjuli08@hotmail.com>
2023-01-09 11:00:34 -0500

    add Passengers component

---
+++ src/components/Passengers.tsx
@@ -0,0 +1,50 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import './Passengers.css'
+
+export const Passengers = ({ state, send }) => {
+  const [value, changeValue] = useState('')
+
+  const onChangeInput = (e: FormEvent<HTMLInputElement>) => {
+    changeValue(e.currentTarget.value)
+
+    console.log(e.currentTarget.value)
+  }
+
+  const goToTicket = () => {
+    send('DONE')
+  }
+
+  const submit = (e: FormEvent) => {
+    e.preventDefault()
+    changeValue('')
+  }
+
+  return (
+    <form onSubmit={submit} className="Passengers">
+      <p className="Passengers-title title">
+        Agrega a las personas que van a volar ✈️
+      </p>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Escribe el nombre completo"
+        required
+        value={value}
+        onChange={onChangeInput}
+      />
+      <div className="Passengers-buttons">
+        <button className="Passengers-add button-secondary" type="submit">
+          Agregar Pasajero
+        </button>
+        <button
+          className="Passenger-pay button"
+          type="button"
+          onClick={goToTicket}
+        >
+          Ver mi ticket
+        </button>
+      </div>
+    </form>
+  )
+}
