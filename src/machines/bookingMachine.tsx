b62538b34dba45148a2cc5083aea406f1df4712d
juliflorezg <forezjuli08@hotmail.com>
2023-01-09 20:01:43 -0500

    add Xstate typegen for  bookingMachine

--- src/machines/bookingMachine.tsx
+++ src/machines/bookingMachine.tsx
@@ -1,6 +1,16 @@
 import { createMachine } from 'xstate'
 
 const bookingMachine = createMachine({
+  tsTypes: {} as import("./bookingMachine.typegen").Typegen0,
+  schema: {
+    context: {},
+    events: {} as
+      | { type: 'START' }
+      | { type: 'CONTINUE' }
+      | { type: 'DONE' }
+      | { type: 'FINISH' }
+      | { type: 'CANCEL' },
+  },
   id: 'buy flight tickets',
   initial: 'initial',
   states: {
@@ -30,3 +40,97 @@ const bookingMachine = createMachine({
 })
 
 export default bookingMachine
+
+// const bookingMachine2 = createMachine(
+//   {
+//     tsTypes: {} as import('./bookingMachine.typegen').Typegen0,
+//     id: 'buy plane tickets',
+//     initial: 'initial',
+//     schema: {
+//       services: {} as {
+//         fetchCountries: {
+//           data: []
+//         }
+//       },
+//       context: {} as {
+//         passengers: []
+//         selectedCountry: string
+//         countries: []
+//         error: string
+//       },
+//     },
+//     context: {
+//       passengers: [],
+//       selectedCountry: '',
+//       countries: [],
+//       error: '',
+//     },
+//     states: {
+//       initial: {
+//         on: {
+//           START: {
+//             target: 'search',
+//           },
+//         },
+//       },
+//       search: {
+//         on: {
+//           CONTINUE: {
+//             target: 'passengers',
+//             actions: assign((context: any, event: any) => {
+//               context.selectedCountry = event.selectedCountry
+//             }),
+//           },
+//           CANCEL: 'initial',
+//         },
+//         ...fillCountries,
+//       },
+
+//       tickets: {
+//         after: {
+//           5000: {
+//             target: 'initial',
+//             actions: 'cleanContext',
+//           },
+//         },
+//         on: {
+//           FINISH: 'initial',
+//         },
+//       },
+//       passengers: {
+//         on: {
+//           DONE: {
+//             target: 'tickets',
+//             cond: 'moreThanOnePassenger',
+//           },
+//           CANCEL: {
+//             target: 'initial',
+//             actions: 'cleanContext',
+//           },
+//           ADD: {
+//             target: 'passengers',
+//             actions: assign((context: any, event: any) =>
+//               context.passengers.push(event.newPassenger)
+//             ),
+//           },
+//         },
+//       },
+//     },
+//   },
+
+//   {
+//     actions: {
+//       cleanContext: () => {
+//         assign((context: any, event: any) => {
+//           context.selectedCountry = ''
+//           context.passengers = []
+//         })
+//       },
+//     },
+//     guards: {
+//       moreThanOnePassenger: context => {
+//         return context.passengers.length > 0
+//       },
+//     },
+//   }
+// )
