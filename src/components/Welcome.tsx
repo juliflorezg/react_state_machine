e4070a3ba959c700b2be10aa55944b2466a6f92c
juliflorezg <forezjuli08@hotmail.com>
2023-01-09 20:02:07 -0500

    send an event from Welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './Welcome.css'
 
-export const Welcome = ({ send }) => {
+export const Welcome = ({ send }: { send: any }) => {
   const startBooking = () => {
+    // send('START')
     send('START')
   }
 
