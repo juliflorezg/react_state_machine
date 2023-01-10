ae5563bd5c22682c39bcf4c24d6dea40b47f2158
juliflorezg <forezjuli08@hotmail.com>
2023-01-10 18:52:53 -0500

    logging context

--- src/containers/BaseLayout.tsx
+++ src/containers/BaseLayout.tsx
@@ -8,7 +8,7 @@ import './BaseLayout.css'
 export const BaseLayout = () => {
   const [state, send] = useMachine(bookingMachine)
 
-  console.log('Our machine:', state.value)
+  console.log('Our machine:', state.value, state.context)
   // console.log('this matches to true', state.matches('initial'))
   // console.log('this matches to false', state.matches('tickets'))
   // console.log('can execute FINISH', state.can('FINISH'))
